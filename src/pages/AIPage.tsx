77d698162bc8d239704c17d18b6d0d3ba711a882
karimkanafani <karim.kanafani@mail.mcgill.ca>
2024-02-09 12:25:41 -0500

    Styled the AI page a little.

--- src/pages/AIPage.tsx
+++ src/pages/AIPage.tsx
@@ -1,7 +1,15 @@
 import NavBar from "../containers/NavBar";
-import React, { useState } from "react";
-import {View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {LinearGradient} from "expo-linear-gradient";
 
 const AIPage = () => {
   const [inputText, setInputText] = useState("");
@@ -11,50 +19,40 @@ const AIPage = () => {
     setInputText("");
   };
   return (
-       <View >
-    {/* Your main content goes here */}
-    <NavBar />
-    
-    {/* Text Input and Submit Button */}
-    <View >
-      <TextInput
-        placeholder="Type your message..."
-        value={inputText}
-        onChangeText={(text) => setInputText(text)}
-      />
-      <TouchableOpacity onPress={handleSubmit}>
-        <Ionicons name="arrow-up-circle" size={30} color="#007bff" />
-      </TouchableOpacity>
-    </View>
-  </View>
+      <LinearGradient colors={["lightblue", "white"]} style={styles.container}>
+        <View style={styles.container}>
+          {/* Your main content goes here */}
+
+          {/* Text Input and Submit Button */}
+          <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}
+                                style={styles.inputContainer}>
+            <TextInput
+                placeholder="Type your message..."
+                value={inputText}
+                onChangeText={(text) => setInputText(text)}
+            />
+            <TouchableOpacity onPress={handleSubmit}>
+              <Ionicons name="arrow-up-circle" size={30} color="#007bff"/>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </View>
+      </LinearGradient>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   inputContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     justifyContent: "space-between",
-//     paddingHorizontal: 16,
-//     position: "absolute",
-//     bottom: 50, // Adjust the value to position it higher or lower
-//     left: 0,
-//     right: 0,
-//     backgroundColor: "white",
-//   },
-//   textInput: {
-//     flex: 1,
-//     borderBottomWidth: 1,
-//     borderColor: "#007bff",
-//     marginRight: 16,
-//   },
-//   submitButton: {
-//     marginLeft: 16,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 90,
+    marginLeft: 15,
+    marginRight: 15,
+  }
+});
 
 export default AIPage;
 
